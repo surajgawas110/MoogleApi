2cbc3e80e72c99b18bdb9ddf9afcb502ef399735
surajgawas110 <48012652+surajgawas110@users.noreply.github.com>
2021-07-07 23:11:03 +0530

    Update CorsConfig.java

--- src/main/java/com/assignment/Demandtec/config/CorsConfig.java
+++ src/main/java/com/assignment/Demandtec/config/CorsConfig.java
@@ -16,7 +16,7 @@ public class CorsConfig {
 				registry.addMapping("/**")
 				.allowedMethods("GET","POST","PUT","DELETE","OPTIONS")
 				.allowedHeaders("*")
-				.allowedOrigins("*");
+				.allowedOrigins("http://moogle-ui-container");
 			}
 		};
 	}
